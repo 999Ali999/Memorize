bbb4fb96ff6008b9b508ba0ad1e3bcb06b518762
999Ali999 <999alishafiei999@gmail.com>
2025-01-01 20:17:42 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ali Shafiei on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
